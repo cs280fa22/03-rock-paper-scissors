32ab751d755c4a168dca8d73bd4668967c11ab0d
Ali Madooei <alimadooei@gmail.com>
2022-09-06 11:13:31 -0400

    Step 12: Computer plays

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const choices = ["rock", "paper", "scissors"];
+
 function play (event) {
   const userChoice = event.target.id;
-  console.log(userChoice);
+  console.log("user choice is", userChoice);
+
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  const computerChoice = choices[randomNumber];
+  console.log("computer choice is", computerChoice);
 }
