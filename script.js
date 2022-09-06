e225d600ec5f90b5cc9819870eaf6b065630b69d
Ali Madooei <alimadooei@gmail.com>
2022-09-06 11:14:04 -0400

    Step 13: Game logic

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ function play (event) {
   const randomNumber = Math.floor(Math.random() * choices.length);
   const computerChoice = choices[randomNumber];
   console.log("computer choice is", computerChoice);
+
+  if (computerChoice === userChoice) {
+    console.log("Draw!");
+  } else if (computerChoice === "rock" && userChoice === "paper") {
+    console.log("You win!");
+  } else if (computerChoice === "rock" && userChoice === "scissors") {
+    console.log("You lost!");
+  } else if (computerChoice === "paper" && userChoice === "scissors") {
+    console.log("You win!");
+  } else if (computerChoice === "paper" && userChoice === "rock") {
+    console.log("You lose!");
+  } else if (computerChoice === "scissors" && userChoice === "rock") {
+    console.log("You win!");
+  } else if (computerChoice === "scissors" && userChoice === "paper") {
+    console.log("You lose!");
+  }  
 }
